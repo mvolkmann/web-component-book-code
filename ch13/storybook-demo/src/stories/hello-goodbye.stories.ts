5a6750aa77002e27e33523c313cffce2ff1b547b
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-02-10 10:41:13 -0600

    more ch13/storybook-demo

---
+++ ch13/storybook-demo/src/stories/hello-goodbye.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/web-components-vite";
+import { expect, userEvent, waitFor } from "storybook/test";
+import { HelloGoodbye } from "../hello-goodbye.ts";
+import "../hello-goodbye.ts";
+
+const meta: Meta = {
+  component: "hello-goodbye",
+};
+export default meta;
+
+const html = String.raw;
+
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const body = canvasElement.ownerDocument.body;
+    const helloGoodbye = body.querySelector("hello-goodbye");
+    expect(helloGoodbye).toBeInTheDocument();
+    if (!helloGoodbye) return;
+
+    const p = helloGoodbye.shadowRoot?.querySelector("p");
+    expect(p).toBeInTheDocument();
+    if (!p) return;
+
+    expect(p).toHaveTextContent("Hello");
+    await userEvent.click(p);
+    expect(p).toHaveTextContent("Goodbye");
+    await userEvent.click(p);
+    expect(p).toHaveTextContent("Hello");
+  },
+};
+
+export const Named: StoryObj = {
+  args: { name: "Earth" },
+};
