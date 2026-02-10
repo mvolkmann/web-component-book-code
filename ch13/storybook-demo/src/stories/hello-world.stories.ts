6f2b59733b51108589b2aa71f40576a20aef5107
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-02-10 07:52:25 -0600

    more Storybook

--- ch13/storybook-demo/src/stories/hello-world.stories.ts
+++ ch13/storybook-demo/src/stories/hello-world.stories.ts
@@ -1,10 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/web-components-vite";
 import "../hello-world.ts";
 
-const meta: Meta = {
-  title: "Components/HelloWorld",
-  component: "hello-world",
-};
+const meta: Meta = { component: "hello-world" };
 export default meta;
 
 const html = String.raw;
