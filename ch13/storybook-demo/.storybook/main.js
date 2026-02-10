7f1e014906899e9387baf09b48d3265c9ca290b4
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-02-10 10:16:36 -0600

    more ch13/storybook-demo

--- ch13/storybook-demo/.storybook/main.js
+++ ch13/storybook-demo/.storybook/main.js
@@ -1,17 +1,13 @@
-
-
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 const config = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs"
+    "@storybook/addon-docs",
+    "storybook-addon-test-codegen",
   ],
-  "framework": "@storybook/web-components-vite"
+  framework: "@storybook/web-components-vite",
 };
 export default config;
---
+++ ch13/storybook-demo/src/hello-goodbye.ts
@@ -0,0 +1,62 @@
+const html = String.raw;
+
+const template = document.createElement("template");
+template.innerHTML = html`
+  <p><span class="salutation"></span>, <span class="name"></span>!</p>
+`;
+
+export class HelloGoodbye extends HTMLElement {
+  #salutation = "Hello";
+  #name = "World";
+  #span1: HTMLSpanElement | undefined; // assigned in connectedCallback
+  #span2: HTMLSpanElement | undefined; // assigned in connectedCallback
+
+  static get observedAttributes() {
+    return ["name"];
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  attributeChangedCallback(name: string, _oldValue: string, newValue: string) {
+    if (name === "name") this.name = newValue;
+  }
+
+  connectedCallback() {
+    this.shadowRoot?.appendChild(template.content.cloneNode(true));
+    this.#span1 = this.shadowRoot?.querySelector(".salutation")!;
+    this.#span2 = this.shadowRoot?.querySelector(".name")!;
+    this.name = this.getAttribute("name") || this.#name;
+    this.salutation = this.getAttribute("salutation") || this.#salutation;
+    this.shadowRoot?.addEventListener("click", () => {
+      const newValue = this.#salutation === "Hello" ? "Goodbye" : "Hello";
+      this.salutation = newValue;
+    });
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get salutation() {
+    return this.#salutation;
+  }
+
+  set name(value: string) {
+    if (this.#span2) this.#span2.textContent = value;
+    if (value === this.#name) return;
+    this.#name = value;
+    this.setAttribute("name", value);
+  }
+
+  set salutation(value: string) {
+    if (this.#span1) this.#span1.textContent = value;
+    if (value === this.#salutation) return;
+    this.#salutation = value;
+    this.setAttribute("salutation", value);
+  }
+}
+
+customElements.define("hello-goodbye", HelloGoodbye);
