61fd26845cdb74384729ab3e640e8588de931863
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-02-24 14:19:50 -0600

    more ch15/vite-ssr-demo

--- ch15/lit-ssr-demo/vite.config.server.ts
+++ ch15/lit-ssr-demo/vite.config.server.ts
@@ -2,15 +2,18 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  root: "src", // avoids generating dist/index.html
   build: {
     // This avoids deleting outDir client files
     // created using vite.config.js.
     emptyOutDir: false,
     ssr: true,
+    lib: {
+      entry: "server.ts",
+      formats: ["es"],
+    },
+    outDir: "../dist",
     rollupOptions: {
-      input: {
-        server: "src/server.ts",
-      },
       output: {
         // Default is "assets/{name}-{hash}.js" which we do not want.
         entryFileNames: "[name].js",
