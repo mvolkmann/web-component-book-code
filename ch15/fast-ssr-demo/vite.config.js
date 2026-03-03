cb3e246acc6cbef0a3c86a1ca37dce5643877f0f
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-03-03 14:21:17 -0600

    progress

--- ch15/fast-ssr-demo/vite.config.js
+++ ch15/fast-ssr-demo/vite.config.js
@@ -2,24 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: "dist",
     rollupOptions: {
       input: ["src/hello-world.ts", "src/server.ts"],
-      output: {
-        format: "esm",
-      },
     },
-    ssr: {
-      noExternal: [],
-      external: [
-        "@hono/node-server",
-        "stream",
-        "buffer",
-        "http",
-        "https",
-        "fs",
-      ],
-    },
-    target: "node24",
+    ssr: {},
   },
 });
