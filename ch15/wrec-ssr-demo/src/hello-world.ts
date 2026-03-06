ee59079f3f9129deaa10da1885876699090991e6
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-03-05 18:52:17 -0600

    added ch15/wrec-ssr-demo

---
+++ ch15/wrec-ssr-demo/src/hello-world.ts
@@ -0,0 +1,12 @@
+import { html, Wrec } from "wrec";
+
+class HelloWorld extends Wrec {
+  static properties = {
+    name: { type: String, value: "World" },
+  };
+
+  static html = html` <p>Hello, <span>this.name</span>!</p> `;
+}
+
+HelloWorld.define("hello-world");
+export default HelloWorld; // so server.ts can import it
---
+++ ch15/wrec-ssr-demo/src/server.ts
@@ -0,0 +1,24 @@
+import { type Context, Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+import HelloWorld from "./hello-world";
+
+const html = String.raw;
+const app = new Hono();
+
+app.use("/*", serveStatic({ root: "./dist" }));
+
+// This returns HTML that includes server-side rendered FAST components.
+app.get("/greet", async (c: Context) => {
+  const name = c.req.query("name");
+  const template = html`
+    <p>The following components are server-side rendered:</p>
+    <hello-world></hello-world>
+    <hello-world name=${name}></hello-world>
+  `;
+  return c.html(template);
+});
+
+serve(app, (info) => {
+  console.log(`listening on port ${info.port}`);
+});
