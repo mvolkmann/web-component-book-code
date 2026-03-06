8fd47dc1ceaa2fc1f3605c83d562d4f3604b907c
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-03-06 15:53:32 -0600

    more ch15/wrec-ssr-demo

--- ch15/wrec-ssr-demo/dom-setup.js
+++ ch15/wrec-ssr-demo/dom-setup.js
@@ -1,5 +1,5 @@
 import { parseHTML } from "linkedom";
-const { window, document, HTMLElement } = parseHTML("<!DOCTYPE html>");
+const { HTMLElement } = parseHTML("<!DOCTYPE html>");
 global.HTMLElement = HTMLElement;
 global.customElements = {
   get: () => {},
