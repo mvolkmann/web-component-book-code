620953e3d41e4ed35b142f512e00e8146e4c8953
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-03-06 07:09:17 -0600

    progress on ch15/wrec-ssr-demo

--- ch15/wrec-ssr-demo/vite.config.server.js
+++ ch15/wrec-ssr-demo/vite.config.server.js
@@ -2,9 +2,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    emptyOutDir: false,
     rollupOptions: {
-      input: ["src/hello-world.ts", "src/server.ts"],
+      input: "server.ts",
+      output: {
+        format: "es",
+      },
     },
-    ssr: {},
+    ssr: true,
   },
 });
