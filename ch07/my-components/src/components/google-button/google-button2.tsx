f64228d04930b5fdbb97f44f8c06681f8e8aba55
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-03-15 11:05:28 -0500

    more chapter 7 google-button

--- ch07/my-components/src/components/google-button/google-button2.tsx
+++ ch07/my-components/src/components/google-button/google-button2.tsx
@@ -1,5 +1,9 @@
 import { Component, h } from '@stencil/core';
 
+// This version differs from google-button in that it uses
+// an anchor element instead of a button element.
+// That removes the need for event handling, but the
+// point of this book example is to demonstrate event handling.
 @Component({
   tag: 'google-button2',
   styleUrl: 'google-button2.css',
