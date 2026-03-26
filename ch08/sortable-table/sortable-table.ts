f668260ea47df483369f6ea3b4097ee8a4438324
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-03-26 14:27:06 -0500

    more ch08/sortable-table.ts

--- ch08/sortable-table/sortable-table.ts
+++ ch08/sortable-table/sortable-table.ts
@@ -6,7 +6,6 @@ import {
   html,
   observable,
   repeat,
-  type ViewTemplate,
 } from "@microsoft/fast-element";
 
 type LooseObject = Record<string, unknown>;
@@ -85,19 +84,23 @@ const template = html<SortableTable>`
       </tr>
     </thead>
     <tbody>
-      ${x => x.makeRows()}
+      ${repeat(
+        x => x.sortedData,
+        html<LooseObject, SortableTable>`
+          <tr>
+            ${repeat(
+              (_, c) => c.parent.propertyArray,
+              html<string, LooseObject>`
+                <td>${(propName, c) => c.parent[propName]}</td>
+              `,
+            )}
+          </tr>
+        `,
+      )}
     </tbody>
   </table>
   <slot name="footnote"></slot>
 `;
-
-function composeTemplates<T>(templates: Array<ViewTemplate<T>>) {
-  const strings = new Array(templates.length + 1).fill(
-    "",
-  ) as unknown as TemplateStringsArray;
-  Object.defineProperty(strings, "raw", { value: strings });
-  return html<T>(strings, ...templates);
-}
 
 @customElement({ name: "sortable-table", template, styles })
 export class SortableTable extends FASTElement {
@@ -111,27 +114,13 @@ export class SortableTable extends FASTElement {
   @observable propertyArray: string[] = [];
 
   get headingPairs() {
-    return this.headings.split(",").map((heading, index) => ({
-      heading: heading.trim(),
-      property: this.propertyArray[index],
-    }));
-  }
-
-  makeRows() {
-    return composeTemplates(this.sortedData.map(obj => this.makeTr(obj)));
-  }
-
-  makeTd(value: unknown) {
-    return html`<td>${value}</td>`;
-  }
-
-  makeTr(obj: LooseObject) {
-    const cells = composeTemplates(
-      this.propertyArray.map(propName => this.makeTd(obj[propName])),
-    );
-    return html`<tr>
-      ${cells}
-    </tr>`;
+    return this.headings
+      .split(",")
+      .map((heading, index) => ({
+        heading: heading.trim(),
+        property: this.propertyArray[index],
+      }))
+      .filter(pair => pair.heading && pair.property);
   }
 
   #sort() {
