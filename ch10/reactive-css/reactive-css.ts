f63a28903befcbb0cccf6d275ab519c85a78c316
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-03-28 08:52:34 -0500

    added ch10/reactive-css

---
+++ ch10/reactive-css/reactive-css.ts
@@ -0,0 +1,20 @@
+import { css, html, Wrec } from "wrec";
+
+class ReactiveCSS extends Wrec {
+  static properties = {
+    size: { type: Number, value: 18 },
+  };
+
+  static css = css`
+    p {
+      font-size: this.size + "px";
+    }
+  `;
+
+  static html = html`
+    <input type="range" value:input="this.size" />
+    <p>My size is reactive!</p>
+  `;
+}
+
+ReactiveCSS.define("reactive-css");
