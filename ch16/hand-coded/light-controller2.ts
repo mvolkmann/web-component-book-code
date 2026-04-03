f9888d1734ccdc28017922e3d2d887aa8e24610d
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-04-03 06:12:31 -0500

    more ch16/hand-coded

---
+++ ch16/hand-coded/light-controller2.ts
@@ -0,0 +1,23 @@
+import { html, Wrec } from "wrec";
+import "./traffic-light"; // register custom element
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+class LightController2 extends Wrec {
+  static properties = {
+    light: { type: String, value: "stop" },
+  };
+  static html = html`<traffic-light state="this.light"></traffic-light>`;
+
+  async connectedCallback() {
+    super.connectedCallback();
+    while (true) {
+      const l = this.light;
+      const seconds = l === "stop" ? 3 : l === "yield" ? 1 : 2;
+      await sleep(seconds * 1000);
+      this.light = l === "stop" ? "go" : l === "yield" ? "stop" : "yield";
+    }
+  }
+}
+
+LightController2.define("light-controller2");
