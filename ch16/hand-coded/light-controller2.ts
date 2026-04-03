21f8eb1ee4a9a1f03aa1f3b00c16a7c49b706fe6
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-04-03 15:05:29 -0500

    improved light-controller2.ts

--- ch16/hand-coded/light-controller2.ts
+++ ch16/hand-coded/light-controller2.ts
@@ -9,8 +9,7 @@ class LightController2 extends Wrec {
   };
   static html = html`<traffic-light state="this.light"></traffic-light>`;
 
-  async connectedCallback() {
-    super.connectedCallback();
+  async ready() {
     while (true) {
       const l = this.light;
       const seconds = l === "stop" ? 3 : l === "yield" ? 1 : 2;
