9b31996f9aec4ba382a2051a35396a5213994d50
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-04-02 19:00:30 -0500

    more ch16/hand-coded

--- ch16/hand-coded/light-controller.ts
+++ ch16/hand-coded/light-controller.ts
@@ -1,28 +1,26 @@
 import { html, Wrec } from "wrec";
-import TrafficLight from "./traffic-light";
+import "./traffic-light"; // register custom element
+import type TrafficLight from "./traffic-light";
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-const sleepMap: Record<string, number> = {
-  stop: 3,
-  yield: 1,
-  go: 2,
-};
 
-class MyApp extends Wrec {
+class LightController extends Wrec {
   static html = html`<traffic-light></traffic-light>`;
 
   async connectedCallback() {
+    // Wait for wrec to finish build the DOM.
     await super.connectedCallback();
     const trafficLight = this.shadowRoot?.querySelector(
       "traffic-light",
     ) as TrafficLight;
     if (!trafficLight) return;
     while (true) {
-      const ms = sleepMap[trafficLight.state] * 1000;
-      await sleep(ms);
+      const { state } = trafficLight;
+      const seconds = state === "stop" ? 3 : state === "yield" ? 1 : 2;
+      await sleep(seconds * 1000);
       trafficLight.next();
     }
   }
 }
 
-MyApp.define("my-app");
+LightController.define("light-controller");
