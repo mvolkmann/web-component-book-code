9bec7ed4d4439d8ea215a29d8dfbbb050079d2df
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-04-11 14:59:00 -0500

    more ch03/scoped-custom-elements

--- ch03/scoped-custom-elements/my-scope.ts
+++ ch03/scoped-custom-elements/my-scope.ts
@@ -1,13 +1,28 @@
 class MyScope extends HTMLElement {
   constructor() {
     super();
+
+    // Create a new custom element registry
+    // that will be scoped to this element.
     const registry = new CustomElementRegistry();
+
+    // Find a custom element class that is registered in the global scope.
     const HelloWorld = customElements.get("hello-world");
+
+    // Define a new custom element name in the scoped registry
+    // that uses the custom element class from the global scope.
+    // A motivation for doing this is to avoid name collisions
+    // when multiple web component implementations
+    // use the same custom element name.
     if (HelloWorld) registry.define("hello-name", HelloWorld);
+
+    // Create a shadow root that uses the scoped registry.
     const shadow = this.attachShadow({
       mode: "open",
       customElementRegistry: registry,
     });
+
+    // Use the new custom element name in the current scope.
     shadow.innerHTML = `
       <!-- This custom element name is known in this scope. -->
       <hello-name name="Mars"></hello-name>
