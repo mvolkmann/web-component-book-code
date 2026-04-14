045679f13e6240f20765e2a2e1d3320be6e30aa7
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-04-14 15:45:44 -0500

    added rectangle-area project

---
+++ ch09/rectangle-area/rectangle-area.ts
@@ -0,0 +1,24 @@
+import { css, html, Wrec } from "wrec";
+import "./number-slider.ts";
+
+class RectangleArea extends Wrec {
+  static properties = {
+    width: { type: Number, value: 10 },
+    height: { type: Number, value: 5 },
+    //area: {type: Number, computed: 'this.width * this.height'}
+  };
+
+  static css = css`
+    .area {
+      font-weight: bold;
+    }
+  `;
+
+  static html = html`
+    <number-slider label="Width:" value="this.width"></number-slider>
+    <number-slider label="Height:" value="this.height"></number-slider>
+    <div class="area">Area: <span>this.width * this.height</span></div>
+  `;
+}
+
+RectangleArea.define("rectangle-area");
