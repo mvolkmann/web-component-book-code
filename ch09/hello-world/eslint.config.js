0b0a5d1991a61ad4f2d1bd4de6e7861cc386b623
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-04-19 08:08:14 -0500

    added use of typescript-eslint

--- ch09/hello-world/eslint.config.js
+++ ch09/hello-world/eslint.config.js
@@ -1,8 +1,22 @@
 import html from "@html-eslint/eslint-plugin";
+import tseslint from "typescript-eslint";
 
 export default [
   {
-    files: ["**/*.{js,ts}"],
+    files: ["**/*.js"],
+    plugins: {
+      "@html-eslint": html,
+    },
+    rules: {
+      ...html.configs["flat/recommended"].rules,
+      "@html-eslint/indent": ["error", 2],
+    },
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
     plugins: {
       "@html-eslint": html,
     },
