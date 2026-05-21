e930f562dc9288cda76b6368ec79e358819b036c
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-05-21 13:13:07 -0500

    ch08/reactive-css update

--- ch08/reactive-css/reactive-css.ts
+++ ch08/reactive-css/reactive-css.ts
@@ -1,22 +1,15 @@
-import {
-  css,
-  customElement,
-  FASTElement,
-  html,
-  observable,
-  ref,
-} from "@microsoft/fast-element";
+import { css, customElement, FASTElement, html, observable, ref } from "@microsoft/fast-element";
 
 const template = html<ReactiveCSS>`
   <input
     type="range"
     min="8"
     max="64"
-    :value="${x => x.size}"
-    @input=${(x, c) => x.handleInput(c.event)}
+    :value="${(x) => x.size}"
+    @input=${(x) => x.handleInput()}
     ${ref("slider")}
   />
-  <p style="--font-size: ${x => x.size}px">My size is reactive!</p>
+  <p style="--font-size: ${(x) => x.size}px">My size is reactive!</p>
 `;
 
 @customElement({
@@ -32,7 +25,7 @@ export class ReactiveCSS extends FASTElement {
   @observable size: number = 18;
   slider: HTMLInputElement;
 
-  handleInput(event: Event) {
+  handleInput() {
     this.size = Number(this.slider.value);
   }
 }
