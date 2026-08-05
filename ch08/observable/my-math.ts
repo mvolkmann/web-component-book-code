413ad13e3f2402667d51296f9ef4dac3bcdf42d7
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-08-05 10:46:43 -0500

    updated ch08/observable/my-math.ts

--- ch08/observable/my-math.ts
+++ ch08/observable/my-math.ts
@@ -1,17 +1,11 @@
-import {
-  attr,
-  customElement,
-  FASTElement,
-  html,
-  observable,
-} from "@microsoft/fast-element";
+import { customElement, FASTElement, html, observable } from "@microsoft/fast-element";
 
 const template = html<MyMath>`
-  <div>single = ${x => x.n}</div>
-  <div>double = ${x => x.double}</div>
-  <div>triple = ${x => x.triple}</div>
-  <button @click=${x => (x.n = x.n - 1)}>Decrement</button>
-  <button @click=${x => (x.n = x.n + 1)}>Increment</button>
+  <div>single = ${(x) => x.n}</div>
+  <div>double = ${(x) => x.double}</div>
+  <div>triple = ${(x) => x.triple}</div>
+  <button @click=${(x) => (x.n = x.n - 1)}>Decrement</button>
+  <button @click=${(x) => (x.n = x.n + 1)}>Increment</button>
 `;
 
 @customElement({
@@ -19,7 +13,7 @@ const template = html<MyMath>`
   template,
 })
 export class MyMath extends FASTElement {
-  @attr n: number = 1;
+  @observable n: number = 1;
   //@observable double: number;
   //@observable triple: number;
 
