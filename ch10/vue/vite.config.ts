8ec5b0582f93b657624e5d63bfd146e51cf9dbdd
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-08-06 08:40:17 -0500

    fixed ch10/vue/vite.config.ts

--- ch10/vue/vite.config.ts
+++ ch10/vue/vite.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-});
-/*
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
 export default defineConfig({
   plugins: [
     vue({
@@ -20,4 +12,3 @@ export default defineConfig({
     }),
   ],
 });
-*/
